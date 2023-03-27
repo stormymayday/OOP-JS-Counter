0a6a4d2fc70123b9fa4a332f1d8b792239890ca4
stormymayday <stormymayday7@gmail.com>
2023-03-27 17:59:19 -0600

    Setting up the getElement function

--- app.js
+++ app.js
@@ -4,3 +4,11 @@ function Counter(element, value) {
     console.log(element, value);
 
 }
+
+// Function that gets the element
+function getElement(elementSelection) {
+
+    // Getting the element
+    const element = document.querySelector(selection);
+
+}
