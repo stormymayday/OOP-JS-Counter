d5587541c448ee4debe2dd3d5e5752353b3afd11
stormymayday <stormymayday7@gmail.com>
2023-03-31 19:11:47 -0600

    Set up the Constructor class

--- app-class.js
+++ app-class.js
@@ -1,3 +1,28 @@
+class Counter {
+    constructor(element, value) {
+
+        // Properties
+        this.counter = element;
+        this.value = value;
+        this.resetBtn = element.querySelector('.reset');
+        this.increaseBtn = element.querySelector('.increase');
+        this.decreaseBtn = element.querySelector('.decrease');
+        this.valueDOM = element.querySelector('.value');
+        this.valueDOM.textContent = this.value;
+
+        // Bidning 'this' such that it points to Counter
+        this.increase = this.increase.bind(this);
+        this.decrease = this.decrease.bind(this);
+        this.reset = this.reset.bind(this);
+
+        // Event Listeners
+        this.increaseBtn.addEventListener('click', this.increase);
+        this.decreaseBtn.addEventListener('click', this.decrease);
+        this.resetBtn.addEventListener('click', this.reset);
+
+    }
+}
+
 // getElement - start
 function getElement(selection) {
 
