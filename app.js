0b938bcbd57e45ccbd62d7c83de275e5c0984f49
stormymayday <stormymayday7@gmail.com>
2023-04-26 19:57:03 -0600

    Importing Counter constructor function as CounterFC in app.js

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import Counter from "./utils/class/class.js";
+import { Counter as CounterFC } from "./constructor-function.js";
 import getElement from "./utils/getElement/getElement.js";
 
 // Instantiating the second-counter using Counter class:
